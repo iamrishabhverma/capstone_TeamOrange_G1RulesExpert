dbf31d46fcdc01158320c641da96c99cdf7074ea
kiranlongia <kiranlongia912@gmail.com>
2020-08-11 17:15:14 -0400

    User Dashboard

--- app/src/main/java/com/example/teamorange_capstone_android/UserDashboard.java
+++ app/src/main/java/com/example/teamorange_capstone_android/UserDashboard.java
@@ -1,14 +1,103 @@
 package com.example.teamorange_capstone_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class UserDashboard extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.HashMap;
+
+public class UserDashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    //Drawer Menu
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ImageView menuIcon;
+    String fullName, phoneNo, email, gender, dob,  password;
+    TextView UserDetailsText, UserNameInitials;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_user_dashboard);
+
+        //Menu Hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        menuIcon = findViewById(R.id.menuIcon);
+        UserDetailsText = findViewById(R.id.welcome_user);
+        UserNameInitials = findViewById(R.id.userNameInitial);
+
+        //Start Session
+        SessionManager sessionManager = new SessionManager(this);
+        HashMap<String, String> userDetails = sessionManager.getUserDetailsFromSession();
+
+        //Get all the data from Session
+        String fullName = userDetails.get(SessionManager.KEY_FULL_NAME);
+        String phoneNumber = userDetails.get(SessionManager.KEY_PHONE_NUMBER);
+
+        navigationDrawer();
+
+        //UserNameInitials.setText(fullName.charAt(0));
+        UserDetailsText.setText("Welcome\n\n" + fullName + "\n" + phoneNumber);
+    }
+
+    //Navigation Drawer Functions
+    private void navigationDrawer() {
+
+        //Navigation Drawer
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+
+        menuIcon.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(drawerLayout.isDrawerVisible(GravityCompat.START)){
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                }
+                else{
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+
+        if(drawerLayout.isDrawerVisible(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                startActivity(new Intent(getApplicationContext(), UserDashboard.class));
+                break;
+
+            case R.id.nav_allQuiz:
+                startActivity(new Intent(getApplicationContext(), AllQuizActivity.class));
+                break;
+        }
+        return true;
     }
 }
